8621e18c46f918d8938051246e72b1aad1c9d402
Arno Saine <arno@mowhi.com>
2024-06-11 00:23:49 +0300

    Add `createHttpResponseAssertion` helper

--- examples/remix-project/utils/response.ts
+++ examples/remix-project/utils/response.ts
@@ -1,43 +1,23 @@
-export function valid(
+type HttpResponseAssertion = (
   condition: unknown,
   body?: ConstructorParameters<typeof Response>[0]
-): asserts condition {
-  if (!condition) {
-    throw new Response(body, {
-      status: 400,
-    });
-  }
+) => asserts condition;
+
+function createHttpResponseAssertion(status: number): HttpResponseAssertion {
+  return function (
+    condition: unknown,
+    body?: ConstructorParameters<typeof Response>[0]
+  ): asserts condition {
+    if (!condition) {
+      throw new Response(body, {
+        status,
+      });
+    }
+  };
 }
 
-export function authorized(
-  condition: unknown,
-  body?: ConstructorParameters<typeof Response>[0]
-): asserts condition {
-  if (!condition) {
-    throw new Response(body, {
-      status: 401,
-    });
-  }
-}
-
-export function allowed(
-  condition: unknown,
-  body?: ConstructorParameters<typeof Response>[0]
-): asserts condition {
-  if (!condition) {
-    throw new Response(body, {
-      status: 403,
-    });
-  }
-}
-
-export function found(
-  condition: unknown,
-  body?: ConstructorParameters<typeof Response>[0]
-): asserts condition {
-  if (!condition) {
-    throw new Response(body, {
-      status: 404,
-    });
-  }
-}
+export const valid: HttpResponseAssertion = createHttpResponseAssertion(400);
+export const authorized: HttpResponseAssertion =
+  createHttpResponseAssertion(401);
+export const allowed: HttpResponseAssertion = createHttpResponseAssertion(403);
+export const found: HttpResponseAssertion = createHttpResponseAssertion(404);
