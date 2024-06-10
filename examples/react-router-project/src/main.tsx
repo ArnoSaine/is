0613e500ccc11b298a5b99b3f865bf8fc8191d17
Arno Saine <arno@mowhi.com>
2024-06-11 00:46:23 +0300

    Add basename

--- examples/react-router-project/src/main.tsx
+++ examples/react-router-project/src/main.tsx
@@ -11,58 +11,63 @@ import { loadIs } from "./is";
 import { LoginAndLogout, routes } from "./mockAuth";
 import { authorized } from "./utils/response";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    loader: async (args) => {
-      const is = await loadIs(args);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      loader: async (args) => {
+        const is = await loadIs(args);
 
-      return {
-        __is: is.__values,
-      };
-    },
-    element: (
-      <>
-        <LoginAndLogout />
-        <Outlet />
-      </>
-    ),
-    errorElement: (
-      <>
-        <h1>Oops!</h1>
-        <Link to="/">Home</Link>
-      </>
-    ),
-    children: [
-      {
-        path: "/",
-        element: (
-          <>
-            <h1>Hello World</h1>
-            <Link to="protected">Protected</Link>
-          </>
-        ),
+        return {
+          __is: is.__values,
+        };
       },
-      {
-        path: "protected",
-        loader: async (args) => {
-          const is = await loadIs(args);
-
-          authorized(is({ authenticated: true }));
-
-          return null;
+      element: (
+        <>
+          <LoginAndLogout />
+          <Outlet />
+        </>
+      ),
+      errorElement: (
+        <>
+          <h1>Oops!</h1>
+          <Link to="/">Home</Link>
+        </>
+      ),
+      children: [
+        {
+          path: "/",
+          element: (
+            <>
+              <h1>Hello World</h1>
+              <Link to="protected">Protected</Link>
+            </>
+          ),
         },
-        element: (
-          <>
-            <h1>Protected</h1>
-            <Link to="/">Home</Link>
-          </>
-        ),
-      },
-    ],
-  },
-  ...routes,
-]);
+        {
+          path: "protected",
+          loader: async (args) => {
+            const is = await loadIs(args);
+
+            authorized(is({ authenticated: true }));
+
+            return null;
+          },
+          element: (
+            <>
+              <h1>Protected</h1>
+              <Link to="/">Home</Link>
+            </>
+          ),
+        },
+      ],
+    },
+    ...routes,
+  ],
+  {
+    basename: (import.meta.env.BASENAME ?? "") + "/",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- examples/react-router-project/vite.config.ts
+++ examples/react-router-project/vite.config.ts
@@ -1,7 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
+const base = (process.env.BASENAME ?? "") + "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
 });
